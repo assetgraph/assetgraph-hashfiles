4a1b029f937815ed2e12c439cfeb306affd4d4ef
Peter Müller <munter@fumle.dk>
2019-10-17 01:41:02 +0200

    linting

--- testdata/fullpage/assets/js/web-share.js
+++ testdata/fullpage/assets/js/web-share.js
@@ -1,19 +1,21 @@
 if (typeof navigator.share === 'function') {
-    var shareLinks = document.querySelectorAll('a[href^="https://twitter.com/intent/tweet"');
+  var shareLinks = document.querySelectorAll(
+    'a[href^="https://twitter.com/intent/tweet"'
+  );
 
-    [].forEach.call(shareLinks, function (anchor) {
-        anchor.textContent = 'Share this';
+  [].forEach.call(shareLinks, function(anchor) {
+    anchor.textContent = 'Share this';
 
-        anchor.addEventListener('click', function (e) {
-            var url = new URL(anchor.href);
+    anchor.addEventListener('click', function(e) {
+      var url = new URL(anchor.href);
 
-            navigator.share({
-                title: url.searchParams.get('title'),
-                text: url.searchParams.get('text'),
-                url: url.searchParams.get('url')
-            })
+      navigator.share({
+        title: url.searchParams.get('title'),
+        text: url.searchParams.get('text'),
+        url: url.searchParams.get('url')
+      });
 
-            e.preventDefault();
-        });
+      e.preventDefault();
     });
+  });
 }
